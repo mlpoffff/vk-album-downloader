8f70e0e897f790a44bad1f85c78790051150b364
Maxim Olennikov <123letsplay93@gmail.com>
2026-02-12 21:54:12 +0700

    FIX: скачивание альбомов вида https://vk.com/tag{owner_id} из вкладки "Альбом по ссылке"

--- app/composables/linkParser.ts
+++ app/composables/linkParser.ts
@@ -3,11 +3,21 @@ export const extractVkId = (url: string): string => {
   return cleaned.replace(/\/$/, '')
 }
 export const extractVkIdFromAlbum = (url: string) => {
-  const match = url.match(/album(-?\d+)_(\d+)/)
-  if (!match) return null
-
-  return {
-    id: Number(match[1]),
-    albumId: String(match[2])
+  const albumMatch = url.match(/album(-?\d+)_(\d+)/)
+  if (albumMatch) {
+    return {
+      id: Number(albumMatch[1]),
+      albumId: String(albumMatch[2])
+    }
   }
+
+  const tagMatch = url.match(/tag(\d+)/)
+  if (tagMatch) {
+    return {
+      id: Number(tagMatch[1]),
+      albumId: '-9000'
+    }
+  }
+
+  return null
 }
